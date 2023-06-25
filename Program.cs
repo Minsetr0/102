0115d484e4b4af21571b63c38b51bd6edd6de4ee
Minsetr0 <nesterovmaksim71@mail.ru>
2023-06-25 20:48:01 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿while (true)
+{
+    Console.WriteLine("Введите строку");
+
+    TryToParse();
+    Console.Clear();
+}
+static void TryToParse()
+{
+    string userInput = Console.ReadLine();
+    bool result = int.TryParse(userInput, out var number);
+
+    if (result == true)
+    {
+        Console.WriteLine("Преобразование прошло успешно. Число: " + number);
+    }
+    else
+    {
+        Console.WriteLine("Преобразование прошло неудачно.");
+    }
+}
